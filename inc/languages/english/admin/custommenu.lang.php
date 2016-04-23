d3f87895b9f999f4b0ba4f0dd2d325d7a8518cd4
TerranUlm <dieter@gobbers.de>
2016-04-23 12:54:26 +0200

    initial import

---
+++ inc/languages/english/admin/custommenu.lang.php
@@ -0,0 +1,62 @@
+<?php
+$l['custommenu_title'] = 'Custom Menus';
+$l['custommenu_description'] = 'The Custom Menus Plugin allows you to add/edit/remove menu items from your MyBB forum';
+	
+$l['custommenu_list'] = 'List Menus';
+
+$l['custommenu_add'] = 'Add Menu';
+$l['custommenu_description'] = 'This section allows you to add a new menu to your forum';
+$l['custommenu_menu_added'] = 'Menu added';
+	
+$l['custommenu_edit'] = 'Edit Menu';
+$l['custommenu_delete'] = 'Delete Menu';
+$l['custommenu_menu_edited'] = 'Menu edited';
+	
+$l['custommenu_table_id_name'] = 'Identifier <em>*</em>';
+$l['custommenu_table_title'] = 'Visible Title';
+$l['custommenu_table_link'] = 'URL';
+$l['custommenu_table_icon'] = 'Icon';
+$l['custommenu_table_hovericon'] = 'Hover Icon';
+$l['custommenu_table_alt'] = 'ALT Text';
+$l['custommenu_table_groups'] = 'Permitted Usergroups';
+$l['custommenu_table_ignoregroups'] = 'Ignored Usergroups';
+$l['custommenu_table_order'] = 'Order'; 
+$l['custommenu_table_status'] = 'Status';
+$l['custommenu_table_options'] = 'Options';
+	
+$l['custommenu_table_menuitems'] = "Menu Items";
+$l['custommenu_table_submenuitems'] = "Submenu Items";
+$l['custommenu_no_menus'] = 'No menus have been created yet';
+	
+$l['custommenu_disabled'] = 'Disabled';
+$l['custommenu_enabled'] = 'Enabled';
+	
+$l['custommenu_update'] = 'Update Menu Order';
+	
+$l['custommenu_link'] = 'Link Url: ';
+$l['custommenu_icon'] = 'Icon Url: ';
+$l['custommenu_hovericon'] = 'Hover Icon Url: ';
+$l['custommenu_alt'] = 'ALT Text: ';
+$l['custommenu_groups'] = 'Permitted Groups (comma separated, leave empty for all groups): ';
+$l['custommenu_ignoregroups'] = 'Ignored Groups (comma separated, overrides permitted groups): ';
+$l['custommenu_window'] = "Open New Window";
+	
+$l['custommenu_error_no_title'] = "A menu title is required!";
+$l['custommenu_error_no_link'] = "A menu link is required!";
+$l['custommenu_error_missing_id'] = "Menu ID is missing!";
+
+$l['custommenu_sub_edit'] = 'Edit Sub Menus';
+$l['custommenu_sub_list'] = 'List Sub Menus';
+$l['custommenu_sub_list_menu'] = 'List Sub Menus for Menu "{menuname}"';
+$l['custommenu_sub_list_description'] = 'List of the Sub Menus of the Main Menu "{menuname}". Use the "Options" to modify the Sub Menus or "Add Submenu" for a new Sub Menu';
+$l['custommenu_sub_add'] = 'Add Submenu';
+$l['custommenu_sub_add_description'] = 'Add a new Sub Menus to the Main Menu "{menuname}".';
+$l['custommenu_sub_editsub'] = 'Edit Sub Menu';
+
+$l['custommenu_all_user_groups'] = 'All Usergroups';
+$l['custommenu_no_groups'] = 'No Usergroup';
+$l['custommenu_unregistered_user'] = 'Unregistered / Not Logged In';
+
+$l['custommenu_can_manage_menus'] = 'Can manage the main menu?';
+
+?>
